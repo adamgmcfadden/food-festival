2de7ee0efe1dc153cdbeee7f4af8b57cf8d0f4e7
Adam Girard-McFadden <adam_m_20@hotmail.com>
2021-09-26 17:27:37 -0400

    all done PWA

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
-const webpack = require('webpack');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-// const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
-// const WebpackPwaManifest = require("webpack-pwa-manifest");
-const path = require('path');
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 
-const config = {
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
   entry: {
-    app: './assets/js/script.js',
-    events: './assets/js/events.js',
-    schedule: './assets/js/schedule.js',
-    tickets: './assets/js/tickets.js'
+    app: "./assets/js/script.js",
+    events: "./assets/js/events.js",
+    schedule: "./assets/js/schedule.js",
+    tickets: "./assets/js/tickets.js"
   },
   output: {
-    filename: '[name].bundle.js',
-    path: `${__dirname}/dist`
+    filename: "[name].bundle.js",
+    path: __dirname + "/dist",
   },
   module: {
     rules: [
       {
-        test: /\.(png|jpe?g|gif)$/i,
+        test: /\.jpg$/,
         use: [
           {
             loader: 'file-loader',
@@ -27,7 +27,7 @@ const config = {
               name(file) {
                 return '[path][name].[ext]';
               },
-              publicPath(url) {
+              publicPath: function (url) {
                 return url.replace('../', '/assets/');
               }
             }
@@ -39,30 +39,34 @@ const config = {
       }
     ]
   },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '/'),
+    }
+  },
   plugins: [
     new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery'
+      $: "jquery",
+      jQuery: "jquery"
     }),
     new BundleAnalyzerPlugin({
-      analyzerMode: 'static'
+      analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+    }),
+    new WebpackPwaManifest({
+      name: "Food Event",
+      short_name: "Foodies",
+      description: "An app that allows you to view upcoming food events.",
+      start_url: "../index.html",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      fingerprints: false,
+      inject: false,
+      icons: [{
+        src: path.resolve("assets/img/icons/icon-512x512.png"),
+        sizes: [96, 128, 192, 256, 384, 512],
+        destination: path.join("assets", "icons")
+      }]
     })
-    // new WebpackPwaManifest({
-    //   name: "Food Event",
-    //   short_name: "Foodies",
-    //   description: "An app that allows you to view upcoming food events.",
-    //   background_color: "#01579b",
-    //   theme_color: "#ffffff",
-    //   fingerprints: false,
-    //   inject: false,
-    //   icons: [{
-    //     src: path.resolve("assets/img/icons/icon-512x512.png"),
-    //     sizes: [96, 128, 192, 256, 384, 512],
-    //     destination: path.join("assets", "icons")
-    //   }]
-    // })
   ],
   mode: 'development'
 };
-
-module.exports = config;
